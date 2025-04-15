96e56dafb46765ddcf7c20235c7276f068b56239
Abdul-Althyab <abdul.althyab@gmail.com>
2025-04-15 15:33:14 +0200

    add HomeComponent with template and styles, and create SharedModule for pagination component

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PaginationComponent } from './component/pagination/pagination.component';
+
+
+@NgModule({
+  declarations: [
+    PaginationComponent
+  ],
+  imports: [
+    CommonModule,
+    PaginationModule.forRoot(),
+  ],
+  exports: [
+    PaginationModule,
+    PaginationComponent
+  ]
+})
+export class SharedModule { }
