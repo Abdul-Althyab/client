bcd2a3167dd7faccb4fb239c79742a0acee714b4
Abdul-Althyab <abdul.althyab@gmail.com>
2025-04-22 17:26:48 +0200

    feat: add checkout module and routing, integrate checkout button in basket

--- src/app/shop/product-details/product-details.component.ts
+++ src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../../shared/Models/Product';
 import { ShopService } from '../shop.service';
 import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { BasketService } from '../../basket/basket.service';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +14,13 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent implements OnInit {
   product: IProduct;
   MainImage: string;
-  constructor(private shopService: ShopService, private route: ActivatedRoute) {}
+  quantity: number = 1;
+  constructor(
+    private shopService: ShopService,
+    private route: ActivatedRoute,
+    private toast: ToastrService,
+    private basketService: BasketService
+  ) {}
   ngOnInit(): void {
     this.loadProductDetails();
   }
@@ -27,5 +35,37 @@ export class ProductDetailsComponent implements OnInit {
   }
   changeMainImage(image: string) {
     this.MainImage = image;
+  }
+
+  incrementQuantity() {
+    if (this.quantity < 10) {
+      this.quantity++;
+      this.toast.success('Incremented quantity to ' + this.quantity, 'Success');
+    } else {
+      this.toast.warning('You can only add 10 items to the cart', 'Enough');
+    }
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+      this.toast.success('Decremented quantity to ' + this.quantity, 'Success');
+    } else {
+      this.toast.warning('You can not decrement below 1 item.', 'Error');
+    }
+  }
+
+  addToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+    this.toast.success(
+      'Added ' + this.quantity + ' items to the basket',
+      'Success'
+    );
+  }
+
+  calculateDiscount(oldPrice: number, newPrice: number): number {
+    return parseFloat(
+      Math.round(((oldPrice - newPrice) / oldPrice) * 100).toFixed(1)
+    );
   }
 }
