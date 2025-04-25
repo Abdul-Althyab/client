2b80b0f849748944503e4366082c5b59e023a54a
Abdul-Althyab <abdul.althyab@gmail.com>
2025-04-25 14:24:56 +0200

    feat: add identity module with routing and register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,9 +14,10 @@ const routes: Routes = [
     path:'basket', loadChildren: () => import('./basket/basket.module')
     .then(m => m.BasketModule)
   },
+  
   {
-    path:'checkout', loadChildren: () => import('./checkout/checkout.module')
-    .then(m => m.CheckoutModule)
+    path:'Account', loadChildren: () => import('./identity/identity.module')
+    .then(m => m.IdentityModule)
   },
   {path:'**',redirectTo:'',pathMatch:'full'}
 ];
